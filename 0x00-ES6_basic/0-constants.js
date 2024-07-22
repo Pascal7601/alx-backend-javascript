916ffb08132aa6597dd8f78b33e496a66a44be55
Pascal7601 <levispascal63@gmail.com>
2024-07-22 21:55:18 +0300

    changes

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,18 +1,15 @@
-/*
-* using const and let keywords
-*/
 export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
-  }
+}
   
 export function getLast() {
     return ' is okay';
-  }
+}
   
 export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
   
     return combination;
-  }
+}
