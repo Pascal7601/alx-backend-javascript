d787e55d5667d369e8c2ad2976bf61828919be9d
Pascal7601 <levispascal63@gmail.com>
2024-07-25 06:45:44 +0000

    primise all methods

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,7 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((data) => {
     const [photo, user] = data;
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
   });
 }
