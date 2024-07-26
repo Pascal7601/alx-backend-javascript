b7e51e6baf3d9d9e66b3bc68b9ba34bfb221f9ac
Pascal7601 <levispascal63@gmail.com>
2024-07-26 13:53:26 +0000

    class methods

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -4,6 +4,28 @@ export default class Currency {
     this._name = name;
   }
 
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('currency name must be string');
+    }
+    return this._name;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('currency code must be string');
+    }
+    return this._code;
+  }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
